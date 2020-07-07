267a1eff3fce34f4a570c357272b59fd42c65b8f
bryan-teng <teng.weiyan.bryan@gmail.com>
2020-07-07 09:51:38 +0800

    Added in an export method

--- src/objects.py
+++ src/objects.py
@@ -238,19 +238,31 @@ class Fund:
 
         df.plot(figsize=(12,4))
         plt.show()
+    
+    def export_to_csv(self, export_name='data/historical-paper-values.csv'):
+        """
+        Exports the DataFrame containing the historical paper values of all the assets
+        """
+        self.all_assets_normalised.to_csv(
+            export_name, index=True, columns=list(self.all_assets_normalised.columns)
+        )
 
 #Test Code
 
-equity1 = Equity('EBAY','2020-06-24',1000)
-equity2 = Equity('ADSK','2020-06-23',500)
+date1 = '2020-05-18'
 
-fund1= Fund(200000,'^NDX','2020-06-19')
+fund1 = Fund(2375706,'^FTSE',date1)
 
-#print(fund1.all_assets.head())
-
-fund1.buy_equity('EBAY','2020-06-24',1000,equity1.historical_prices.loc['2020-06-24','close'])
-fund1.buy_equity('ADSK','2020-06-23',500,equity2.historical_prices.loc['2020-06-23','close'])
+fund1.buy_equity('GSK.L',date1,397,1670.20)
+fund1.buy_equity('SGE.L',date1,802,653)
+fund1.buy_equity('NG.L',date1,394,922.2)
+fund1.buy_equity('WHR.L',date1,3345,100.5)
+fund1.buy_equity('SSE.L',date1,161,1242.50)
+fund1.buy_equity('RHIM.L',date1,69,2306.00)
+fund1.buy_equity('ICP.L',date1,64,1109.00)
+fund1.buy_equity('ASC.L',date1,21,2768.8)
 
 #print(fund1.all_assets.head())
 print(fund1.all_assets_normalised)
 fund1.plot_fund_performance()
+fund1.export_to_csv()
